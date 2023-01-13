8164d8e5ccc548a4e959acd7ec1353b7617b0cbb
nerdy-cuber <daniel.cheng.wang@gmail.com>
2023-01-13 17:44:21 +1100

    Finished drop down feature, can select sessions.

---
+++ main.py
@@ -0,0 +1,41 @@
+import re
+import matplotlib.pyplot as plt
+import tkinter as tk
+import numpy as np
+from ast import literal_eval
+from tkinter import filedialog
+
+
+cstimerfile = filedialog.askopenfilename(title="Please choose a .txt file to open:")  # opens file
+
+re_sessions = re.compile(r'session\d+":(\[]|.+?]])')
+re_names = re.compile(r'name\\":\\"(.+?)\\')
+re_ranks = re.compile(r'rank\\":(\d+)')
+with open(cstimerfile) as f:
+    file = f.read()
+    ranks = re_ranks.findall(file)
+    ranks = [int(rank) for rank in ranks]  # convert ranks from str to int
+    names = re_names.findall(file)  # list of names
+    sessions = re_sessions.findall(file)  # list of sessions
+    sessions = [np.array([session, name]) for session, name in zip(sessions, names)]  # list of arrays of sessions + name
+    sessions = np.array([session for _, session in sorted(zip(ranks, sessions))])  # array of ranked sessions
+
+
+
+master = tk.Tk()
+master.title("Please choose a session:")
+m = tk.StringVar()
+master.geometry("300x100")
+
+
+def get_input():  # select session
+    global session
+    session = sessions[np.where(sessions==m.get())[0]][0]  # find corresponding session of name
+    master.destroy()
+
+
+menu = tk.OptionMenu(master, m, *sessions[:, 1])  # get column 2 of sessions list
+menu.pack()
+submit_button = tk.Button(master, text="Submit", command=get_input)
+submit_button.pack()
+tk.mainloop()
