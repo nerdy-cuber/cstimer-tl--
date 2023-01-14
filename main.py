4087a2bf28135fcab5c806bfa0e3e63165161699
nerdy-cuber <daniel.cheng.wang@gmail.com>
2023-01-14 14:51:09 +1100

    Remove regex, add support for .json files.

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-import re
 import matplotlib.pyplot as plt
 import tkinter as tk
-import numpy as np
 from ast import literal_eval
 from tkinter import filedialog
 from datetime import datetime as dt
+import json
 
 
 # Get data from file
-cstimerfile = filedialog.askopenfilename(title="Please choose a .txt file to open:")  # opens file
+cstimerfile = filedialog.askopenfilename(title="Please choose a .txt or .json file to open:")  # opens file
 
-re_sessions = re.compile(r'session\d+":(\[]|.+?]])')
-re_names = re.compile(r'name\\":\\"(.+?)\\')
-re_ranks = re.compile(r'rank\\":(\d+)')
+
 with open(cstimerfile) as f:
-    file = f.read()
-    ranks = re_ranks.findall(file)
-    ranks = [int(rank) for rank in ranks]  # convert ranks from str to int
-    names = re_names.findall(file)  # list of names
-    sessions = re_sessions.findall(file)  # list of sessions
-    sessions = [np.array([session, name]) for session, name in zip(sessions, names)]  # list of arrays of sessions + name
-    sessions = np.array([session for _, session in sorted(zip(ranks, sessions))])  # array of ranked sessions
-
+    file = json.loads(f.read())
+    sessions = list(file.values())
+    sessions.pop()
+    properties = file["properties"]["sessionData"]  # extracts properties value from file, which contains all the names and ranks
+    properties = properties.replace("\\", "")  # reformats string
+    properties = properties.replace("null", "None")  # reformats string
+    properties = literal_eval(properties)  # converts string to dict
+    names = [item["name"] for item in properties.values()]  # extracts names
+    ranks = [item["rank"] for item in properties.values()]  # extracts ranks
+    names = [name for _, name in sorted(zip(ranks, names))]  # list of ranked names
+    sessions = [session for _, session in sorted(zip(ranks, sessions))]  # list of ranked sessions
 
 # Select the session
 master = tk.Tk()
@@ -30,15 +30,13 @@ m = tk.StringVar()
 master.geometry("300x100")
 
 
-def get_input():  # Get session choice
+def get_input():  # Get name choice
     global session
-    session = sessions[np.where(sessions==m.get())[0]]  # find corresponding session of name
-    session = session.reshape(2)
-    session = session[0]
+    session = sessions[names.index(m.get())]  # Finds corresponding session with name
     master.destroy()
 
 
-menu = tk.OptionMenu(master, m, *sessions[:, 1])  # get column 2 of sessions list
+menu = tk.OptionMenu(master, m, *names)
 menu.pack()
 submit_button = tk.Button(master, text="Submit", command=get_input)
 submit_button.pack()
@@ -46,7 +44,6 @@ tk.mainloop()
 
 
 # plotting thing
-session = literal_eval(session)
 times = [sum(solve[0])/1000 for solve in session]  # extracts times
 timestamps = [dt.fromtimestamp(solve[3]) for solve in session]  # extracts timestamps
 
